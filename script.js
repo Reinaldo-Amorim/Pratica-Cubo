460edde8cba5c96f7319ee37a2b67867dbd9e78b
Reinaldo <reinaldo.amorim@oulook.pt>
2023-09-07 12:58:37 -0300

    Treinamento ed Git

---
+++ script.js
@@ -0,0 +1,6 @@
+let numero1 = prompt('escreva um numero para fazer a soma');
+let numero2 = prompt('escreva um numero para fazer a soma');
+
+let resultado = numero1 + numero2
+
+alert(`O resultado é ${resultado}`)
